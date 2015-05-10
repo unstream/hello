ed11f432eb459e57bc3771286f2f9f3698c072f1
unstream <unstream@web.de>
2015-05-10 23:14:29 +0200

    Build example as a servlet

--- hello/hello/src/main/java/com/test/hello/domain/User.java
+++ hello/hello/src/main/java/com/test/hello/domain/User.java
@@ -1,14 +1,17 @@
 package com.test.hello.domain;
-
+/**
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+*/
 public class User {
+/*
 	@Id
     @GeneratedValue(strategy=GenerationType.AUTO)
+*/
     private long id;
 	private String firstName;
 	private String lastName;
