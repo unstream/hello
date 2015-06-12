68f6da98155219cbdeae2940f55d50d28a87c50f
unstream <unstream@web.de>
2015-06-12 17:37:32 +0200

    Add support for colors

--- hello/hello/src/main/java/com/test/hello/domain/Fractal.java
+++ hello/hello/src/main/java/com/test/hello/domain/Fractal.java
@@ -8,7 +8,28 @@ public class Fractal {
 	int width = 500;
 	int height = 500;
 	int iterations = 100;
+	String color1 = "#000000";
+	String color2 = "#00aabb";
+	String color3 = "#ffffff";
 
+	public String getColor1() {
+		return color1;
+	}
+	public void setColor1(String color1) {
+		this.color1 = color1;
+	}
+	public String getColor2() {
+		return color2;
+	}
+	public void setColor2(String color2) {
+		this.color2 = color2;
+	}
+	public String getColor3() {
+		return color3;
+	}
+	public void setColor3(String color3) {
+		this.color3 = color3;
+	}
 	public double getC0() {
 		return c0;
 	}
